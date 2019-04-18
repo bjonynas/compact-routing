6805d01dac3a93fe8799de0a313ee5e10cb166d1
Benas Jonynas <bjonynas@gmail.com>
2019-04-18 14:59:15 +0100

    Update get_kcore.py

--- bin/get_kcore.py
+++ bin/get_kcore.py
@@ -1,10 +1,11 @@
 import networkx as nx
 import sys
 import os
+import math
 
 graph = nx.Graph()
 filename = sys.argv[1]
-sourceFile = open('./results/stage1/' + filename, 'r')
+sourceFile = open('./results/stage1/data-' + filename + '.dat', 'r')
 
 line = sourceFile.readline()
 line = sourceFile.readline()
@@ -24,16 +25,26 @@ while line != '':
     line = sourceFile.readline()
     
 sourceFile.close()
-kc = nx.algorithms.core.k_core(graph)
 
-filename = filename.split('-')
+kc = nx.algorithms.core.k_shell(graph)
+
+shellSizes = []
+nodesInShell = kc.number_of_nodes()
+shellSizes.append(nodesInShell)
+x = 1
+
+#calculate k-core shells until a shell with at least root(n log(n)) is found
+while nodesInShell < math.sqrt(graph.number_of_nodes() * math.log(graph.number_of_nodes())):
+    kc = nx.algorithms.core.k_shell(graph, x)
+    nodesInShell = kc.number_of_nodes()
+    shellSizes.append(nodesInShell)
+    x = x + 1
 
 if not os.path.exists('./results/stage2'):
     os.makedirs('./results/stage2')
 
-saveFile = open('./results/stage2/kcore-' + filename[1] +'-' + filename[2], 'w+')
-
-saveFile.write('kcore size: ' + str(kc.number_of_nodes()) + '\n')
+saveFile = open('./results/stage2/kcore-' + sys.argv[1] + '.dat', 'w+')
+saveFile.write('kcore sizes: ' + str(shellSizes) + '\n')
 for node in kc.nodes():
     saveFile.write(str(node) + '\n')
 saveFile.close()
